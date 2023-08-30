4c05a44b770140a24bf91dd26d8e7821a8ab9500
DatTran <trancongdat.datct@gmail.com>
2023-08-31 00:21:31 +0700

    add child => parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-angular';
+  children = [
+    {
+      childAge: 18,
+      childName: "Cong Dat 18"
+    },
+    {
+      childAge: 19,
+      childName: "Cong Dat 19"
+    },
+    {
+      childAge: 20,
+      childName: "Cong Dat 20"
+    },
+  ]
 }
