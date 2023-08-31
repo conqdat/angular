30fe1ed0a74bac9178755b6160a0643ddfb0ecb1
DatTran <trancongdat.datct@gmail.com>
2023-08-31 15:56:20 +0700

    add pipe

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LearnAngular
+
+Custom pipe:
+- create & export a class that implement PipeTransform
+- In this class put @pipe to mark to this is a pipe
+- Declare class in AppModule
+- Use it as normal pipe
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-angular';
+  protected readonly Date = Date;
+
+  person = {
+    name: "Cong Dat",
+    age: 22
+  }
 }
