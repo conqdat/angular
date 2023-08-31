5fb116f1b4c91c927144a67d9c77e5dc8fdbcad5
DatTran <trancongdat.datct@gmail.com>
2023-08-31 15:27:22 +0700

    add code for ngIf & ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-angular';
+  count = 0;
+  authors = [
+    {
+      id: 1,
+      firstName: 'Flora',
+      lastName: 'Tell',
+      email: 'ftwell0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.180.237.33',
+    },
+    {
+      id: 2,
+      firstName: 'Priscella',
+      lastName: 'Signe',
+      email: 'psigne1@berkeley.edu',
+      gender: 'Female',
+      ipAddress: '183.243.228.65',
+    },
+  ];
+
+  handleIncreaseCount() {
+    this.count++
+  }
+  handleDecreaseCount() {
+    this.count--
+  }
 }
