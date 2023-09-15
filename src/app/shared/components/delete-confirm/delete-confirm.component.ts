5398fc4fe5f6d680710d8a31a28a569fd86324c9
Dat Tran <khiemtran21012000@gmail.com>
2023-09-15 17:52:44 +0700

    add code for SnackBar

--- src/app/shared/components/delete-confirm/delete-confirm.component.ts
+++ src/app/shared/components/delete-confirm/delete-confirm.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {PostService} from "../services/post.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-delete-confirm',
@@ -12,12 +13,17 @@ export class DeleteConfirmComponent {
   constructor(
     private postService: PostService,
     public dialogRef: MatDialogRef<DeleteConfirmComponent>,
+    private _snackBar: MatSnackBar,
     @Inject(MAT_DIALOG_DATA) public data: { postId: number }
   ) {
   }
 
   handleDeletePost() {
     this.postService.deletePost(this.data.postId).subscribe(result => {
+      this._snackBar.open("Deleted successful !", "OK", {
+        verticalPosition: "top",
+        horizontalPosition: "center",
+      })
       this.postService.getPosts()
     })
   }
